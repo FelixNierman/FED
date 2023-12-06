0e257fbedc8e6e2e4076e8cbb881e4c3b1264c97
FelixNierman <150920547+FelixNierman@users.noreply.github.com>
2023-12-06 22:15:50 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
-// JavaScript Document
-console.log("hi");
+
+
+var openButton = document.querySelector("header nav button");
+
+openButton.onclick = openMenu;
+
+function openMenu() {
+    var nav = document.querySelector("header nav:nth-of-type(2)");
+    nav.classList.toggle("showMenu");
+
+    var animationButton1 = document.querySelector("header nav button svg:nth-of-type(1)");
+    animationButton1.classList.toggle("animation1");
+
+    var animationButton2 = document.querySelector("header nav button svg:nth-of-type(2)");
+    animationButton2.classList.toggle("animation2");
+
+
+}
